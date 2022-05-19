252c04f6fa6a43b5ca368a5a072ba6b0b1768e06
moncho <50428+moncho@users.noreply.github.com>
2022-05-20 00:58:42 +0200

    chore: change project layout
    To facilitate using this project as a package, code is now organized in three
    folders, cmd, internal and pkg. Code that can be used by others can be bound
    under pkg.
    
    Also, updated Go version to 1.18 and upgraded deps and tools.
    
    Signed-off-by: moncho <50428+moncho@users.noreply.github.com>

--- pkg/warp/warp_benchmark_test.go
+++ pkg/warp/warp_benchmark_test.go
@@ -7,7 +7,7 @@ func BenchmarkParallelSeedGeneration(b *testing.B) {
 	pass := "uyVkW5vKXX3RpvnUcj7U3Q"
 	salt := "zXrlmk3p5Lxr0vjJKdcJWQ"
 	for i := 0; i < b.N; i++ {
-		parallelSeedGeneration(pass, salt)
+		_, _ = parallelSeedGeneration(pass, salt)
 	}
 }
 
@@ -15,6 +15,6 @@ func BenchmarkSerialSeedGeneration(b *testing.B) {
 	pass := "uyVkW5vKXX3RpvnUcj7U3Q"
 	salt := "zXrlmk3p5Lxr0vjJKdcJWQ"
 	for i := 0; i < b.N; i++ {
-		serialSeedGeneration(pass, salt)
+		_, _ = serialSeedGeneration(pass, salt)
 	}
 }
